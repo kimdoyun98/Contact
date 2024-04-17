e324d3e9ac185c2df65a52289ea05f8020626321
kimdoyun98 <rladlsxo1250@gmail.com>
2024-04-18 06:25:55 +0900

    Home Fragment 기본 틀 (디자인)

--- app/src/main/java/com/example/contact/ui/home/Home.kt
+++ app/src/main/java/com/example/contact/ui/home/Home.kt
@@ -7,35 +7,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.contact.R
+import com.example.contact.databinding.FragmentHomeBinding
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GetTokenResult
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Home.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Home : Fragment() {
     private val tag = "Home"
+    private lateinit var binding: FragmentHomeBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.i(tag, "HomeFragment")
+        binding = FragmentHomeBinding.bind(view)
+
+
 
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+    ): View = FragmentHomeBinding.inflate(layoutInflater).root
 }

1c9977845394c1c3fbdced15442e33d143fe2d06
kimdoyun98 <rladlsxo1250@gmail.com>
2024-05-03 22:22:10 +0900

    Detail Plan (1)

--- app/src/main/java/com/example/contact/ui/plan/detail/PlanDetail.kt
+++ app/src/main/java/com/example/contact/ui/plan/detail/PlanDetail.kt
@@ -2,11 +2,34 @@ package com.example.contact.ui.plan.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import com.example.contact.R
+import com.example.contact.data.user.UserInfo
+import com.example.contact.databinding.ActivityPlanDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
+import io.github.horaciocome1.fireflow.asFlow
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class PlanDetail : AppCompatActivity() {
+    private lateinit var binding: ActivityPlanDetailBinding
+    private val viewModel: PlanDetailViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_plan_detail)
+        binding = ActivityPlanDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val planId = intent.getStringExtra("planId")!!
+        viewModel.setPlanId(planId)
+
+        binding.viewmodel = viewModel
+        binding.lifecycleOwner = this
+
+        viewModel.getPlanData().observe(this){
+            binding.plan = it!!
+            viewModel.getMemberDisplayName(it.member)
+        }
     }
 }
---
+++ app/src/main/java/com/example/contact/ui/plan/detail/PlanDetailViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.contact.ui.plan.detail
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.contact.data.plan.Plan
+import com.example.contact.data.user.UserInfo
+import com.example.contact.util.firebase.FirebaseRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.github.horaciocome1.fireflow.asFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class PlanDetailViewModel @Inject constructor(
+    private val firebaseRepository: FirebaseRepository
+): ViewModel() {
+    private var planId: String? = null
+    private val _memberList = MutableLiveData<String>()
+    val memberList:LiveData<String> = _memberList
+
+    fun setPlanId(planId: String){
+        this.planId = planId
+    }
+
+    fun getPlanData(): LiveData<Plan?> = firebaseRepository.getPlan(planId!!).asFlow<Plan>().asLiveData()
+
+    fun getMemberDisplayName(memberUid: ArrayList<String>){
+        val member = StringBuilder()
+        memberUid.forEach { uid ->
+            viewModelScope.launch {
+                firebaseRepository.getUserInfo(uid).asFlow<UserInfo>().collect{
+                    val displayName = it!!.displayName.toString()
+                    member.append("$displayName ")
+                    Log.e("viewModel member1", member.toString())
+                    _memberList.value = member.toString()
+                }
+            }
+        }
+    }
+}
